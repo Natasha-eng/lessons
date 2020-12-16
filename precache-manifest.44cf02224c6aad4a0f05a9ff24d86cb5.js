8571e200d33f746ab6dc4bccd5540142f2808820
Natasha-eng <natasha.shilova.89@gmail.com>
2020-12-16 13:33:51 +0300

    Updates

---
+++ precache-manifest.44cf02224c6aad4a0f05a9ff24d86cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf62549b71546b55cee8decc14f0e353",
+    "url": "/lessons/index.html"
+  },
+  {
+    "revision": "85f8680124d7e789048d",
+    "url": "/lessons/static/css/main.33921b11.chunk.css"
+  },
+  {
+    "revision": "bf3c0f2a71846b69657e",
+    "url": "/lessons/static/js/2.f7bbef87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lessons/static/js/2.f7bbef87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f8680124d7e789048d",
+    "url": "/lessons/static/js/main.185c578e.chunk.js"
+  },
+  {
+    "revision": "e2d6ccf25a994a527e15",
+    "url": "/lessons/static/js/runtime-main.91fb55e6.js"
+  }
+]);
